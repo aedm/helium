f33349d8d8f0b6117d038e2e269b4b41a4935a1b
Gábor Gyebnár <korteur@gmail.com>
2020-09-09 18:05:49 +0200

    mut is fun

--- app/src/main.rs
+++ app/src/main.rs
@@ -13,14 +13,13 @@ fn main() {
     let f2 = new_aref(Node::new(Box::new(FloatNode::new())));
     let adder = new_aref(Node::new(Box::new(Adder::new())));
 
-    let mut f1p = &mut *f1.borrow_mut();
-    let mut f2p = &mut *f2.borrow_mut();
-    let mut a1s = &mut *adder.borrow_mut();
+    let f1p = &mut *f1.borrow_mut();
+    let f2p = &mut *f2.borrow_mut();
+    let a1s = &mut *adder.borrow_mut();
     connect_slot(&mut a1s.slots[0], &mut f1p.providers[0]);
     connect_slot(&mut a1s.slots[0], &mut f2p.providers[0]);
 
     a1s.inner.run();
 
     println!("Hi");
-
 }
