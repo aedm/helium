529fd728c024da88d02f7f218e2a7007f93e8a51
Gábor Gyebnár <korteur@gmail.com>
2020-09-09 18:02:10 +0200

    Deref is fun

--- app/src/main.rs
+++ app/src/main.rs
@@ -13,9 +13,9 @@ fn main() {
     let f2 = new_aref(Node::new(Box::new(FloatNode::new())));
     let adder = new_aref(Node::new(Box::new(Adder::new())));
 
-    let mut f1p = (&f1 as &RefCell<Node>).borrow_mut();
-    let mut f2p = (&f2 as &RefCell<Node>).borrow_mut();
-    let mut a1s = (&adder as &RefCell<Node>).borrow_mut();
+    let mut f1p = &mut *f1.borrow_mut();
+    let mut f2p = &mut *f2.borrow_mut();
+    let mut a1s = &mut *adder.borrow_mut();
     connect_slot(&mut a1s.slots[0], &mut f1p.providers[0]);
     connect_slot(&mut a1s.slots[0], &mut f2p.providers[0]);
 
